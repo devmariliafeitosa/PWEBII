f569b5ab4d07bb7b6d0eec09afc3acab48e8d2c1
Marilia <168124353+MariliaFeitosa@users.noreply.github.com>
2026-02-02 08:43:26 -0300

    Primeira prática React do Semestre II

---
+++ Semestre_II/Praticas_react/react-app-01/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+import FormIMC from "./components/FormIMC";
+
+export default function App() {
+
+  return (
+    <>
+     <FormIMC title={"Saiba seu IMC"} />
+     <FormIMC title={"Calcule seu IMC"} />
+    </>
+  );
+}
+
